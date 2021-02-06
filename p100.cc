903652d7761fb39af6c904ee974fd62712595a76
junan <kfggw@outlook.com>
2021-02-06 14:59:25 +0800

    p100: 相同的树

---
+++ p100.cc
@@ -0,0 +1,36 @@
+
+/**
+ * @brief 树节点定义.
+ *
+ */
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+  /**
+   * @brief 通过Preorder遍历比较树的节点.
+   *
+   * @param p
+   * @param q
+   * @return true
+   * @return false
+   */
+  bool isSameTree(TreeNode *p, TreeNode *q) {
+    if (p == q) {
+      return true;
+    } else if (p == nullptr && q != nullptr || p != nullptr && q == nullptr ||
+               p->val != q->val) {
+      return false;
+    } else {
+      return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+  }
+};
