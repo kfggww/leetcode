8e60b5a4b471463113bfa27b2c812af6e61ddb6e
junan <kfggw@outlook.com>
2021-02-08 06:47:08 +0800

    p101: 递归方法判断对称二叉树

--- p101.cc
+++ p101.cc
@@ -19,7 +19,7 @@ struct TreeNode {
  * 思路:
  * 使用层次遍历, 检查是否每层的节点是对称的.
  */
-class Solution {
+class Solution0 {
 public:
   bool isSymmetric(TreeNode *root) {
     if (root == nullptr)
@@ -67,9 +67,54 @@ public:
     return true;
   }
 };
+
 /**
- * 总结:
- * 栈不能检查是否对称, 括号匹配可以用栈检查, 但对称和括号匹配是不同的.
- * TODO:
- * 此解法空间复杂度过大, 考虑递归求解.
+ * @brief 使用递归方法求解.
+ *
+ * 思路:
+ * 对于根的左子树, 按照"根->左->右"的顺序遍历
+ * 对于根的右子树, 按照"根->右->左"的顺序遍历
+ * 如果树是对称的, 两种方式应该得到相同的序列.
  */
+class Solution1 {
+public:
+  bool isSymmetric(TreeNode *root) {
+    if (root == nullptr)
+      return true;
+    exchange(root->right);
+    return isSame(root->left, root->right);
+  }
+
+  /**
+   * @brief 递归交换左右子树.
+   *
+   * @param root
+   */
+  void exchange(TreeNode *root) {
+    if (root == nullptr)
+      return;
+    exchange(root->left);
+    exchange(root->right);
+    TreeNode *tmp = root->left;
+    root->left = root->right;
+    root->right = tmp;
+  }
+
+  /**
+   * @brief 判断两个树是否相同, 递归. 根相同 && 左子树相同 && 右子树相同
+   *
+   * @param root1
+   * @param root2
+   * @return true
+   * @return false
+   */
+  bool isSame(TreeNode *root1, TreeNode *root2) {
+    if (root1 == root2)
+      return true;
+    if (root1 == nullptr && root2 != nullptr ||
+        root1 != nullptr && root2 == nullptr || root1->val != root2->val)
+      return false;
+    return isSame(root1->left, root2->left) &&
+           isSame(root1->right, root2->right);
+  }
+};
