f727bbb40b0c014fccbe58510c9180445a06b8d8
junan <kfggw@outlook.com>
2021-02-11 08:29:33 +0800

    p104: 二叉树的深度

---
+++ p104.cc
@@ -0,0 +1,17 @@
+#include "tree_node.h"
+
+/**
+ * @brief 二叉树的深度
+ * 
+ */
+class Solution {
+public:
+  int maxDepth(TreeNode *root) {
+    if (root == nullptr)
+      return 0;
+    int leftDepth = maxDepth(root->left);
+    int rightDepth = maxDepth(root->right);
+    // ?: 优先级有坑
+    return 1 + (leftDepth >= rightDepth ? leftDepth : rightDepth);
+  }
+};
