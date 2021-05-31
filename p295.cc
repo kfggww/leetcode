eb326f32db6ab789b786ecb6f18895dc613b8708
kfggww <kfggww@outlook.com>
2021-05-31 12:54:27 +0800

    p295: 使用两个堆计算数据流的中位数.

--- p295.cc
+++ p295.cc
@@ -1,6 +1,8 @@
 #include <algorithm>
+#include <functional>
 #include <iostream>
 #include <map>
+#include <queue>
 #include <vector>
 using std::cout;
 using std::endl;
@@ -12,10 +14,10 @@ using std::vector;
  * 思路: 使用一个map记录每个数字出现的个数.
  * TODO: 超时, 问题出在每次计算中位数都要排序.
  */
-class MedianFinder {
+class MedianFinder0 {
 public:
   /** initialize your data structure here. */
-  MedianFinder() : _total(0) {}
+  MedianFinder0() : _total(0) {}
 
   void addNum(int num) {
     if (_recorder.find(num) != _recorder.end())
@@ -55,6 +57,46 @@ private:
   int _total;
 };
 
+/*
+ * 改进解法:
+ *
+ * 维护两个堆, 分别保存前一半和后一半的元素, 计算中位数时使用两个堆顶元素即可.
+ */
+class MedianFinder {
+public:
+  MedianFinder() {}
+
+  void addNum(int num) {
+    // 确定新的num放到哪个堆中, 必须要保证两个堆顶是有序的, 否则求解的结果不对.
+    if (_lo.size() == 0 || num <= _lo.top())
+      _lo.push(num);
+    else
+      _hi.push(num);
+
+    // ATTENTION: 必须做类型转换, 因为size_t是无符号类型的, 直接使用会溢出.
+    if ((int)_lo.size() - (int)_hi.size() > 1) {
+      _hi.push(_lo.top());
+      _lo.pop();
+    } else if ((int)_hi.size() - (int)_lo.size() > 1) {
+      _lo.push(_hi.top());
+      _hi.pop();
+    }
+  }
+
+  double findMedian() {
+    if (_lo.size() > _hi.size())
+      return _lo.top();
+    else if (_lo.size() < _hi.size())
+      return _hi.top();
+    else
+      return 0.5 * (_lo.top() + _hi.top());
+  }
+
+private:
+  std::priority_queue<int, vector<int>, std::less<int>> _lo; // 较小的一半
+  std::priority_queue<int, vector<int>, std::greater<int>> _hi; // 较大的一半
+};
+
 /**
  * Your MedianFinder object will be instantiated and called as such:
  * MedianFinder* obj = new MedianFinder();
@@ -64,7 +106,7 @@ private:
 
 int main() {
   MedianFinder obj;
-  obj.addNum(3);
+  obj.addNum(1);
   obj.addNum(2);
   cout << obj.findMedian() << endl;
   obj.addNum(3);
